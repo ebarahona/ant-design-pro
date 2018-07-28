25089f76df6def13fb9ec7263625c2b4f4665c98
Ed Barahona <ebarahona@gmail.com>
2018-07-28 02:11:40 -0700

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -2,4 +2,9 @@
 module.exports = {
   // 'primary-color': '#10e99b',
   'card-actions-background': '#f5f8fa',
+    // hugah blue: #3366ff;
+  // 'ant-card-body': {
+  //   border-radius: 4px;
+  //   box-shadow: 0 7px 14px 0 rgba(50,50,93,.1), 0 3px 6px 0 rgba(0,0,0,.07);
+  // }
 };
