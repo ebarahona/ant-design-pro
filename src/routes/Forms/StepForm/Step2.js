c1dcfca5cf38f86145bfb274d19c0f3236b8a9d0
Ed Barahona <ebarahona@gmail.com>
2018-07-28 03:51:34 -0700

    Update Step2.js

--- src/routes/Forms/StepForm/Step2.js
+++ src/routes/Forms/StepForm/Step2.js
@@ -41,19 +41,19 @@ class Step2 extends React.PureComponent {
         <Alert
           closable
           showIcon
-          message="确认转账后，资金将直接打入对方账户，无法退回。"
+          message="After confirming the transfer, the funds will be directly credited to the other party's account and cannot be returned."
           style={{ marginBottom: 24 }}
         />
-        <Form.Item {...formItemLayout} className={styles.stepFormText} label="付款账户">
+        <Form.Item {...formItemLayout} className={styles.stepFormText} label="Payment account">
           {data.payAccount}
         </Form.Item>
-        <Form.Item {...formItemLayout} className={styles.stepFormText} label="收款账户">
+        <Form.Item {...formItemLayout} className={styles.stepFormText} label="Accounts Receivable">
           {data.receiverAccount}
         </Form.Item>
-        <Form.Item {...formItemLayout} className={styles.stepFormText} label="收款人姓名">
+        <Form.Item {...formItemLayout} className={styles.stepFormText} label="Payee name">
           {data.receiverName}
         </Form.Item>
-        <Form.Item {...formItemLayout} className={styles.stepFormText} label="转账金额">
+        <Form.Item {...formItemLayout} className={styles.stepFormText} label="Transfer amount">
           <span className={styles.money}>{data.amount}</span>
           <span className={styles.uppercase}>
             （
@@ -62,13 +62,13 @@ class Step2 extends React.PureComponent {
           </span>
         </Form.Item>
         <Divider style={{ margin: '24px 0' }} />
-        <Form.Item {...formItemLayout} label="支付密码" required={false}>
+        <Form.Item {...formItemLayout} label="Payment pin" required={false}>
           {getFieldDecorator('password', {
             initialValue: '123456',
             rules: [
               {
                 required: true,
-                message: '需要支付密码才能进行支付',
+                message: 'A pin is required to send payment',
               },
             ],
           })(<Input type="password" autoComplete="off" style={{ width: '80%' }} />)}
@@ -85,10 +85,10 @@ class Step2 extends React.PureComponent {
           label=""
         >
           <Button type="primary" onClick={onValidateForm} loading={submitting}>
-            提交
+            Submit
           </Button>
           <Button onClick={onPrev} style={{ marginLeft: 8 }}>
-            上一步
+            Previous
           </Button>
         </Form.Item>
       </Form>
